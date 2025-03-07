06496a8e7864e964a75bf5b5f46d130d398e7683
InDaZZ <f-k-87@list.ru>
2025-03-07 11:35:37 +0300

    black

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="center">
+        <div className="black-square"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
